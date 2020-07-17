7fae12cb52b502a8f1cb6140f35fd457e7ae0d71
Alexandra1509 <54314439+Alexandra1509@users.noreply.github.com>
2020-07-17 17:52:27 +0300

    English_teacher_documents

---
+++ js/file.js
@@ -0,0 +1,8 @@
+menu.onclick = function myFunction(){
+var x = document.getElementById('myMenu');
+
+if (x.className === "menu") {
+  x.className += " responsive";
+} else {x.className = "menu";
+}
+}
